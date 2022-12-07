f74f066929266a2696ca3b7c159d1372ce484053
Zofia Salata <zs429590@students.mimuw.edu.pl>
2022-12-07 20:08:54 +0100

    working version with example data

--- ekf_slam.py
+++ ekf_slam.py
@@ -11,11 +11,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # EKF state covariance
-Cx = np.diag([0.05, 0.05, np.deg2rad(30.0)]) ** 2
-Qx = np.diag([0.05, 0.05, 0.5]) ** 2
+Cx = np.diag([0.5, 0.5, np.deg2rad(30.0)]) ** 2
+#Qx = np.diag([0.05, 0.05, 0.5]) ** 2
 
 #  Simulation parameter -> expected noise based on device parameteres?
-Q_sim = np.diag([0.05, np.deg2rad(1.0), 0.02]) ** 2
+Q_sim = np.diag([0.2, np.deg2rad(1.0), 0.02]) ** 2
 R_sim = np.diag([1.0, np.deg2rad(10.0)]) ** 2
 
 # landmarks in our world
@@ -29,7 +29,7 @@ SIM_TIME = 50.0  # simulation time [s]
 MAX_RANGE = 4.0  # maximum observation range
 M_DIST_TH = 2.0  # Threshold of Mahalanobis distance for data association.
 STATE_SIZE = 3  # State size [x,y,yaw]
-LM_SIZE = 3  # LM state size [x,y,z]
+LM_SIZE = 2  # LM state size [x,y,z]
 
 show_animation = True
 
@@ -104,10 +104,7 @@ def calc_landmark_position(x, z):
 
     zp[0, 0] = x[0, 0] + z[0] * math.cos(x[2, 0] + z[1])
     zp[1, 0] = x[1, 0] + z[0] * math.sin(x[2, 0] + z[1])
-    zp[2, 0] = z[2] # Z coord doesnt change since robot is not moving in Z  
-    
-    print("calc_landmark_position:")
-    print(zp)
+    #zp[2, 0] = z[2] # Z coord doesnt change since robot is not moving in Z  
 
     return zp
 
@@ -119,23 +116,22 @@ def get_landmark_position_from_state(x, ind):
 # lm landmark position [x,y,z] where lm is based on previous observations and z is our new observation
 def calc_innovation(lm, xEst, PEst, z, LMid):
     posZ = 0 # robots z coord doesnt change is always 0
-    delta = lm[0:2] - xEst[0:2]
+    delta = lm - xEst[0:2]
     q = (delta.T @ delta)[0, 0] # length of delta squared 
     z_angle = math.atan2(delta[1, 0], delta[0, 0]) - xEst[2, 0] # angle between our front and line from as to the landmark
-    zp = np.array([[math.sqrt(q), pi_2_pi(z_angle),lm[2]]]) # landmark from our perspective - how should it be based on previous observations
+    zp = np.array([[math.sqrt(q), pi_2_pi(z_angle)]]) # landmark from our perspective - how should it be based on previous observations
     y = (z - zp).T # difference between our new observation and 
     y[1] = pi_2_pi(y[1])
     H = jacob_h(q, delta, xEst, LMid + 1)
-    S = H @ PEst @ H.T + Qx
+    S = H @ PEst @ H.T +Cx[0:2,0:2] # Qx
 
     return y, S, H
 
 
 def jacob_h(q, delta, x, i):
     sq = math.sqrt(q)
-    G = np.array([[-sq * delta[0, 0], - sq * delta[1, 0], 0, sq * delta[0, 0], sq * delta[1, 0],0],
-                  [delta[1, 0], - delta[0, 0], - q, - delta[1, 0], delta[0, 0],0],
-                  [0,0,0,0,0,1]])
+    G = np.array([[-sq * delta[0, 0], - sq * delta[1, 0], 0, sq * delta[0, 0], sq * delta[1, 0]],
+               [delta[1, 0], - delta[0, 0], - q, - delta[1, 0], delta[0, 0]],])
 
     G = G / q
     nLM = calc_n_lm(x)
@@ -149,7 +145,6 @@ def jacob_h(q, delta, x, i):
 
     return H
 
-
 def pi_2_pi(angle):
     return (angle + math.pi) % (2 * math.pi) - math.pi
 
@@ -157,7 +152,7 @@ def pi_2_pi(angle):
 # we assume observations are sorted by index
 def new_movement_observations():
     posX = 0 
-    posY = new_movement_observations.iterator # in every move it moves 1 in Y direction
+    posY = (0.1)*new_movement_observations.iterator # in every move it moves 1 in Y direction
     posZ = 0
     posAngle = np.pi/2 
     # add noise to gps x-y
@@ -182,13 +177,13 @@ def new_movement_observations():
             dn = d + np.random.randn() * Q_sim[0, 0] ** 0.5  # observed distance (real with noise)
             angle_n = angle + np.random.randn() * Q_sim[1, 1] ** 0.5  # observed angke (real with noise)
             dzn = dz + np.random.randn() * Q_sim[2, 2] ** 0.5
-            #zi = np.array([dn, angle_n, dzn, i])
-            zi = np.array([d, angle, dz, i])
+            zi = np.array([dn, angle_n, i])
+            #zi = np.array([d, angle, dz, i])
             z = np.vstack((z, zi))
     
     new_movement_observations.iterator += 1
 
-    u = np.array([0.7,0])
+    u = np.array([1,0])
     
     # add noise to movement so its how we think we're moving (info from wheels or sth)
     u_with_noise = np.array([[
@@ -217,10 +212,8 @@ def main():
     while SIM_TIME >= time:
         time += DT
         u, z = new_movement_observations()
-        print(u)
-        print(z)
 
-        #predicted position based only on movement
+        # predicted position based only on movement
         xDR = motion_model(xDR, u)
 
         xEst, PEst = ekf_slam(xEst, PEst, u, z)
