fb7c07825a6789bbcb4b95f398cbbce1fd0ccfa0
Zofia Salata <zs429590@students.mimuw.edu.pl>
2022-12-15 11:47:34 +0100

    final version

--- image_processing.py
+++ image_processing.py
@@ -3,12 +3,19 @@ import numpy as np
 import cv2
 import find_matches_with_superglue as matches
 
-intrinsic = np.matrix([[1.10137417e+03, 0.00000000e+00, 2.89506647e+02],
- [0.00000000e+00, 1.07474733e+03, 1.84718234e+02],
- [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]],float)
+# intrinsic = np.matrix([[1.10137417e+03, 0.00000000e+00, 2.89506647e+02],
+#  [0.00000000e+00, 1.07474733e+03, 1.84718234e+02],
+#  [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]],dtype = np.float64)
 
-distortion = np.matrix([[ 2.33589358e+00, -6.07874111e+01, -2.29399415e-02,  8.79756112e-02, -1.45708167e-01]], float)
+# distortion = np.matrix([[ 7.46597504e-01,  7.89619631e+01, -2.26703831e-02,  1.18290186e-01,
+#   -3.06426901e+03]], dtype = np.float64)
 
+intrinsic = np.matrix([[915.18878362,   0.,         239.58328874],
+ [  0.,         911.09442479, 194.90084703],
+ [  0.,          0.,           1.        ]], dtype = np.float64)
+
+distortion = np.matrix([[-4.36931096e-01,  2.52126992e+00, -7.28634641e-03, -5.15932592e-03,
+  -2.62649369e+01]], dtype = np.float64)
 
 def getRotationsInXY(angle):
     Rz = np.zeros(shape = (3, 3))
@@ -26,13 +33,13 @@ def getRotationsInXY(angle):
 def calculatePoints3D(points1, points2, distance):
     
     #calculate extrinsic matrix
-    rotation1 = np.identity(4)
+    rotation1 = np.identity(4, dtype = np.float64)
     rotation1[:3, :3] = getRotationsInXY(-distance[2]) # with minus ?
-    rotation2 = np.identity(4)
+    rotation2 = np.identity(4, dtype = np.float64)
 
-    translation1 = np.identity(4)
+    translation1 = np.identity(4, dtype = np.float64)
     translation1[:2, 3] = [value * -1 for value in distance] [:2]
-    translation2 = np.identity(4)
+    translation2 = np.identity(4, dtype = np.float64)
 
     extrinsic1 = np.linalg.inv(rotation1 @ translation1)
     extrinsic2 = np.linalg.inv(rotation2 @ translation2)
@@ -44,15 +51,22 @@ def calculatePoints3D(points1, points2, distance):
     projectionMatrix1 = intrinsic @ extrinsic1
     projectionMatrix2 = intrinsic @ extrinsic2
 
-    return cv2.triangulatePoints(projectionMatrix1, projectionMatrix2, points1[:,:15], points2[:,:15])
+    points_undist1 = cv2.undistortPoints(points1.astype(np.float64), intrinsic, distortion)
+    points_undist2 = cv2.undistortPoints(points2.astype(np.float64), intrinsic, distortion)
+    
+    print(points_undist1)
+    print(points_undist2)
+    return cv2.triangulatePoints(projectionMatrix1, projectionMatrix2, points1.astype(np.float64).T, points2.astype(np.float64).T)
 
     
 
 def main():
     (points1, points2) = matches.find_matches()
-    X = calculatePoints3D(points1.T,points2.T,[5,0,0])
-    X //= X[3]
-    print(X.T)
+    print(points1, points2)
+    X = calculatePoints3D(points1[:10,:],points2[:10,:],[5.0,0.0,0.0])
+    #X //= X[3]
+    X = cv2.convertPointsFromHomogeneous(X.T)
+    print(X)
 
 
 if __name__ == '__main__':
