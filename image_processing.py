51f19866f9b1c110a3d3f19c4a9fa8472f400927
Zofia Salata <zs429590@students.mimuw.edu.pl>
2022-12-15 06:46:10 +0100

    adding finding matches

--- image_processing.py
+++ image_processing.py
@@ -1,11 +1,13 @@
 from gettext import translation
 import numpy as np
 import cv2
-intrinsic = np.matrix([[1.09939263e+03, 0.00000000e+00, 2.82665849e+02],
-    [0.00000000e+00, 1.08260264e+03, 1.81292903e+02],
-    [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
+import find_matches_with_superglue as matches
 
-distortion = np.matrix([[ 2.33589358e+00, -6.07874111e+01, -2.29399415e-02,  8.79756112e-02, -1.45708167e-01]])
+intrinsic = np.matrix([[1.10137417e+03, 0.00000000e+00, 2.89506647e+02],
+ [0.00000000e+00, 1.07474733e+03, 1.84718234e+02],
+ [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]],float)
+
+distortion = np.matrix([[ 2.33589358e+00, -6.07874111e+01, -2.29399415e-02,  8.79756112e-02, -1.45708167e-01]], float)
 
 
 def getRotationsInXY(angle):
@@ -29,7 +31,7 @@ def calculatePoints3D(points1, points2, distance):
     rotation2 = np.identity(4)
 
     translation1 = np.identity(4)
-    translation1[:2, 3] = distance[:2]
+    translation1[:2, 3] = [value * -1 for value in distance] [:2]
     translation2 = np.identity(4)
 
     extrinsic1 = np.linalg.inv(rotation1 @ translation1)
@@ -42,13 +44,16 @@ def calculatePoints3D(points1, points2, distance):
     projectionMatrix1 = intrinsic @ extrinsic1
     projectionMatrix2 = intrinsic @ extrinsic2
 
-
-    return cv2.triangulatePoints(projectionMatrix1,projectionMatrix2, points1, points2)
+    return cv2.triangulatePoints(projectionMatrix1, projectionMatrix2, points1[:,:15], points2[:,:15])
 
     
 
 def main():
-    calculatePoints3D("hahaha", "elo", "xd")
+    (points1, points2) = matches.find_matches()
+    X = calculatePoints3D(points1.T,points2.T,[5,0,0])
+    X //= X[3]
+    print(X.T)
+
 
 if __name__ == '__main__':
     main()
