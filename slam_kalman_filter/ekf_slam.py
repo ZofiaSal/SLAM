d82c0b261006fe8eb5a9cb8ff7327e124c80085e
Piotr Kamiński <67481570+Laplasjan107@users.noreply.github.com>
2023-04-19 21:13:48 +0200

    adjusted ekf for 3d points

--- slam_kalman_filter/ekf_slam.py
+++ slam_kalman_filter/ekf_slam.py
@@ -12,6 +12,7 @@ import numpy as np
 
 # EKF state covariance
 Cx = np.diag([0.5, 0.5, np.deg2rad(30.0)]) ** 2
+Cx2 = np.diag([0.5, 0.5, 0.5]) ** 2
 #Qx = np.diag([0.05, 0.05, 0.5]) ** 2
 
 #  Simulation parameter -> expected noise based on device parameteres?
@@ -29,9 +30,31 @@ SIM_TIME = 50.0  # simulation time [s]
 MAX_RANGE = 4.0  # maximum observation range
 M_DIST_TH = 2.0  # Threshold of Mahalanobis distance for data association.
 STATE_SIZE = 3  # State size [x,y,yaw]
-LM_SIZE = 4  # LM state size [x,y,z]
+LM_SIZE = 3  # LM state size [x,y,z]
 
 show_animation = True
+
+def search_correspond_landmark_id(xAug, PAug, zi):
+     """
+     Landmark association with Mahalanobis distance
+     """
+
+     nLM = calc_n_lm(xAug)
+
+     min_dist = []
+
+     for i in range(nLM):
+         lm = get_landmark_position_from_state(xAug, i)
+         y, S, H = calc_innovation(lm, xAug, PAug, zi, i)
+         min_dist.append(y.T @ np.linalg.inv(S) @ y)
+
+     min_dist.append(M_DIST_TH)  # new landmark
+
+     min_id = min_dist.index(min(min_dist))
+
+     return min_id
+
+
 
 # xEst -> [robot's state,landmarks' state]
 def ekf_slam(xEst, PEst, u, z):
@@ -45,7 +68,8 @@ def ekf_slam(xEst, PEst, u, z):
 
     # Update
     for iz in range(len(z[:, 0])):  # for each observation
-        id = z[iz, LM_SIZE]
+        #id = z[iz, LM_SIZE]
+        id = search_correspond_landmark_id(xEst, PEst, z[iz, 0:LM_SIZE])
 
         nLM = calc_n_lm(xEst)
         if id == nLM:
@@ -104,13 +128,8 @@ def calc_landmark_position(x, z):
 
     zp[0, 0] = x[0, 0] + z[0] * math.cos(x[2, 0] + z[1])
     zp[1, 0] = x[1, 0] + z[0] * math.sin(x[2, 0] + z[1])
-    #zp[2, 0] = z[2] # Z coord doesnt change since robot is not moving in Z  
+    zp[2, 0] = z[2] # Z coord doesnt change since robot is not moving in Z  
 
-    #return zp
-    zp[0, 0] = z[0]
-    zp[1, 0] = z[1]
-    zp[2, 0] = z[2]
-    zp[3, 0] = z[3]
     return zp
 
 
@@ -121,27 +140,31 @@ def get_landmark_position_from_state(x, ind):
 # lm landmark position [x,y,z] where lm is based on previous observations and z is our new observation
 def calc_innovation(lm, xEst, PEst, z, LMid):
     posZ = 0 # robots z coord doesnt change is always 0
-    estimatedLM = lm[0:2] + lm[3] * [np.cos(lm[2] - xEst[2]), np.sin(lm[2] - xEst[2])]
-    delta = estimatedLM - xEst[0:2]
+    delta = lm - np.matrix([xEst[0,0], xEst[1,0], 0]).T
     q = (delta.T @ delta)[0, 0] # length of delta squared 
     z_angle = math.atan2(delta[1, 0], delta[0, 0]) - xEst[2, 0] # angle between our front and line from as to the landmark
-    zp = np.array([[math.sqrt(q), pi_2_pi(z_angle)]]) # landmark from our perspective - how should it be based on previous observations
+    zp = np.array([[math.sqrt(q), pi_2_pi(z_angle), 0]]) # landmark from our perspective - how should it be based on previous observations
     y = (z - zp).T # difference between our new observation and 
     y[1] = pi_2_pi(y[1])
     H = jacob_h(q, delta, xEst, LMid + 1)
-    S = H @ PEst @ H.T +Cx[0:2,0:2] # Qx
+    S = H @ PEst @ H.T +Cx2#[0:2,0:2] # Qx
 
     return y, S, H
 
 
 def jacob_h(q, delta, x, i):
     sq = math.sqrt(q)
-    G = np.array([[-sq * delta[0, 0], - sq * delta[1, 0], 0, sq * delta[0, 0], sq * delta[1, 0]],
-               [delta[1, 0], - delta[0, 0], - q, - delta[1, 0], delta[0, 0]],])
+    print(delta)
+    G = np.array(
+        [
+            [-sq * delta[0, 0]  , - sq * delta[1, 0], 0     , sq * delta[0, 0]  , sq * delta[1, 0], 0],
+            [delta[1, 0]        , - delta[0, 0]     , - q   , - delta[1, 0]     , delta[0, 0], 0],
+            [0,0,1,0,0, 0]
+        ])
 
     G = G / q
     nLM = calc_n_lm(x)
-    F1 = np.hstack((np.eye(STATE_SIZE), np.zeros((3, LM_SIZE * nLM)))) # 3,3+LM_SIZE * nLM
+    F1 = np.hstack((np.eye(STATE_SIZE), np.zeros((LM_SIZE, LM_SIZE * nLM)))) # 3,3+LM_SIZE * nLM
     F2 = np.hstack((np.zeros((LM_SIZE, STATE_SIZE)), np.zeros((LM_SIZE,  LM_SIZE * (i - 1))),
                     np.eye(LM_SIZE), np.zeros((LM_SIZE, LM_SIZE * (nLM -  i))))) # 3 +2+LM_SIZE * (nLM -1)
 
@@ -174,15 +197,17 @@ def new_movement_observations():
     for i in range(len(RFID[:, 0])):
         dx = RFID[i, 0] - posX
         dy = RFID[i, 1] - posY
+        dz = RFID[i, 2] - posZ 
         d = math.hypot(dx, dy)          #real distance
         angle = pi_2_pi(math.atan2(dy, dx) - posAngle) #real angle
-        
         zi = []
         if d <= MAX_RANGE:
             # counting noise for landmarks position
             dn = d + np.random.randn() * Q_sim[0, 0] ** 0.5  # observed distance (real with noise)
-            angle_n = angle + np.random.randn() * Q_sim[1, 1] ** 0.5  # observed angke (real with noise)
-            zi = np.array([posX, posY, posAngle + angle_n, 1, i])
+            angle_n = angle + np.random.randn() * Q_sim[1, 1] ** 0.5  # observed angle (real with noise)
+            dzn = dz + np.random.randn() * Q_sim[2, 2] ** 0.5
+            zi = np.array([dn, angle_n, dz, i])
+            #zi = np.array([d, angle, dz, i])
             z = np.vstack((z, zi))
     
     new_movement_observations.iterator += 1
@@ -204,7 +229,7 @@ def main():
 
     # State Vector [x y yaw v]'
     xEst = np.zeros((STATE_SIZE, 1)) # Gauss mean
-    xEst[2,0]= np.pi/2 # only for our simulation
+    xEst[2,0]= np.pi/2 # only for our symulation
     PEst = np.eye(STATE_SIZE)       # covariance
 
     xDR = xEst  # Dead reckoning -> simulated position with noise (without observations)
